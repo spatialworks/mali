0156546369241f2552deeb969951e9f0dcb6a805
Ernest Guevarra <ernest@validmeasures.org>
2019-04-11 22:13:21 +0100

    update createHexSticker.R

--- inst/createHexSticker.R
+++ inst/createHexSticker.R
@@ -3,5 +3,5 @@ library(hexSticker)
 sticker("inst/figures/mali.png",
         s_x = 1, s_y = 1, s_width = 0.8,
         package = "", p_color = "#D84942", p_y = 1.4,
-        h_size = 1, h_color = "#D84942", h_fill = NA,
+        h_size = 1.5, h_color = "#D84942", h_fill = NA,
         filename = "man/figures/mali.png")
